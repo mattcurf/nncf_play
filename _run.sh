5d88e5962bff53d3904fe610f16adb4815e8f27f
Matt Curfman <matt@tecnovelty.com>
2024-09-19 11:00:12 -0700

    Update to OpenVINO 2024.4 and latest NPU and GPU runtimes

--- _run.sh
+++ _run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-source /opt/intel/openvino_2024.2.0/setupvars.sh
+source /root/miniforge3/bin/activate ov
+source /opt/intel/openvino_2024.4.0/setupvars.sh
 
-python3 ./nncf-quant.py
+cd /tmp
+python3 /project/nncf-quant.py
 
 echo "CPU fp32"
 /root/openvino_cpp_samples_build/intel64/Release/benchmark_app -d CPU -niter 1000 -m output/resnet50_fp32.xml | grep Throughput
@@ -16,6 +18,10 @@ if [ "$HAS_GPU" == "1" ]; then
 fi
 
 if [ "$HAS_NPU" == "1" ]; then
+  # Workaround for defect https://github.com/intel/linux-npu-driver/issues/44
+  sed -i "s/type=\"MaxPool\" version=\"opset14\"/type=\"MaxPool\" version=\"opset8\"/g"  output/resnet50_fp32.xml
+  sed -i "s/type=\"MaxPool\" version=\"opset14\"/type=\"MaxPool\" version=\"opset8\"/g"  output/resnet50_int8.xml
+
   echo "NPU fp32"
   /root/openvino_cpp_samples_build/intel64/Release/benchmark_app -d NPU -niter 1000 -m output/resnet50_fp32.xml | grep Throughput
   echo "NPU int8"
